39b6b34f0600e5be9964ff821f1fd9f7ce6e2ba0
Sebastian Cameroni <sebastian.cameroni@hotmail.com>
2024-11-27 16:58:07 -0300

    Correcciones

--- app/views.py
+++ app/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import redirect, render
 from .layers.services import services
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-from django.contrib.auth import authenticate, login  #authenticate para verificar credenciales / login para iniciar sesión
 
 
 def index_page(request):
@@ -13,8 +12,8 @@ def index_page(request):
 # esta función obtiene 2 listados que corresponden a las imágenes de la API y los favoritos del usuario, y los usa para dibujar el correspondiente template.
 # si el opcional de favoritos no está desarrollado, devuelve un listado vacío.
 def home(request):
-    images = services.getAllImages() #Función getAllImages que retorna una lista con las card.
-    favourite_list = services.getAllFavourites(request) #función getAllFavourites de services. esta función retorna los favs del usuario en formato card.
+    images = services.getAllImages()  #Función getAllImages que retorna una lista con las card.
+    favourite_list = services.getAllFavourites(request)  #Función getAllFavourites de services. esta función retorna los favs del usuario en formato card.
 
     return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
 
@@ -24,7 +23,7 @@ def search(request):
     # si el texto ingresado no es vacío, trae las imágenes y favoritos desde services.py,
     # y luego renderiza el template (similar a home).
     if (search_msg != ''):
-        images = services.getAllImages(input=search_msg) #Usa la función getAllImages de la carpeta service usando como parametro la variable search_msg.
+        images = services.getAllImages(input=search_msg)  #Usa la función getAllImages de la carpeta service usando como parametro la variable search_msg.
         return render(request, 'home.html', {'images': images})
     else:
         return redirect('home')
@@ -33,8 +32,8 @@ def search(request):
 # Estas funciones se usan cuando el usuario está logueado en la aplicación.
 @login_required
 def getAllFavouritesByUser(request):
-    favourite_list = services.getAllFavourites(request) #traigo la función que retorna la lista de favs del usuario desde services.
-    return render(request, 'favourites.html', { 'favourite_list': favourite_list }) #lo muestro en el templates favourites.html
+    favourite_list = services.getAllFavourites(request)  #Traigo la función que retorna la lista de favs del usuario desde services.
+    return render(request, 'favourites.html', { 'favourite_list': favourite_list }) #Lo muestro en el templates favourites.html
 
 @login_required
 def saveFavourite(request):
@@ -44,9 +43,9 @@ def saveFavourite(request):
 @login_required
 def deleteFavourite(request):
     delete = services.deleteFavourite(request) #Desde services traigo la función que me permite borrar un favorito por su ID.
-    return redirect('/favourites/') #Lo redirigo a la plantilla de favoritos.
+    return redirect('/favourites/')  #Lo redirigo a la plantilla de favoritos.
 
 @login_required
 def exit(request):
-    logout(request)                             #Función de Django para cerrar sesion
-    return render(request, 'index.html')        #Vuelve al index.html
+    logout(request)  #Función de Django para cerrar sesion
+    return render(request, 'index.html')  #Vuelve al index.html
